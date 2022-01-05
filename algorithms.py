2bb2c94b942100765abbc68d103717a7e96e1ece
Bruce Rose <brucej.rose@orange.fr>
2022-01-05 10:02:58 +0100

    Tests ok with clone in algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -2,6 +2,7 @@ import random
 from container import hash_ind
 
 def clone_ind(ind, icls, scls):
+    """Clones an individual, does the same as toolbox.clone"""
     indcopy = icls(i for i in ind) # Individual
     indcopy.strategy = scls(i for i in ind.strategy) # Strategy
     assert ind is not indcopy, "copy " + str(indcopy) + "\nis the same ref as\n" + str(ind)
@@ -53,13 +54,15 @@ def varOr(population, toolbox, parents, lambda_, cxpb, mutpb, *clone_args):
         if op_choice < cxpb:            # Apply crossover
             ind1, ind2 = list(map(toolbox.clone, random.sample(population, 2)))
 
-            ancestor = toolbox.clone(ind1) # BUG toolbox.clone doesn't make a new instance? Why?
-            # ancestor = clone_ind(ind1, *clone_args) # BUG toolbox.clone doesn't make a new instance? Why?
+            ancestor = toolbox.clone(ind1) 
+            # toolbox.clone doesn't make a new instance? Why?
+            # ==> Actually yes it does
+            # ancestor = clone_ind(ind1, *clone_args) # Useless
 
 
-            ### TEST REMOVE ME: Test OK with clone_ind
-            ancestor.curiosity = 666.0
-            assert ind.curiosity != 666.0
+            ### TEST REMOVE ME: Test OK with clone_ind and test OK with toolbox.clone
+            # ancestor.curiosity = 666.0
+            # assert ind.curiosity != 666.0
             ### END TEST
 
             ind1, ind2 = toolbox.mate(ind1, ind2)
@@ -71,16 +74,16 @@ def varOr(population, toolbox, parents, lambda_, cxpb, mutpb, *clone_args):
             ind = toolbox.clone(random.choice(population))
 
             ancestor = toolbox.clone(ind)
-            # ancestor = clone_ind(ind, *clone_args) # BUG toolbox.clone doesn't make a new instance? Why?
+            # ancestor = clone_ind(ind, *clone_args) # Useless
 
             ind, = toolbox.mutate(ind)
             # assert ancestor != ind # Doesn't pass... is it even a problem? 
             # No because '==' checks the values and 'is' checks the identities
             assert ancestor is not ind
 
-            ### TEST REMOVE ME: The test passes!
-            ancestor.curiosity = 666.0
-            assert ind.curiosity != 666.0
+            ### TEST REMOVE ME: The test passes! and test OK with toolbox.clone
+            # ancestor.curiosity = 666.0
+            # assert ind.curiosity != 666.0
             ### END TEST
 
             del ind.fitness.values

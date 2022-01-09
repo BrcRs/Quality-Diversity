6b491498d11487a6b86060e0e043dec19b6b4e64
Bruce Rose <brucej.rose@orange.fr>
2022-01-09 16:32:08 +0100

    There are now 2 plot functions, one for our work and one for the robot arm environment from the sferes2 framework

--- plot.py
+++ plot.py
@@ -22,8 +22,31 @@ def recup(fichier, nb_gen):
 
   return size,sum_qual,sum_nov,max_qual
 
-def main():
+def recup_robotarm(fichier):
+  size=[]
+  max_qual=[]
+  sum_nov=[]
+  sum_qual=[]
+  f = open(fichier, "r")
+  myline = f.readline()
+  i = 0
+  while myline != '':
+    myline = f.readline()
+    if ("##" not in myline) and (myline != ''):
+      x= myline.split(' ')
+    #   print (len(x),x)
+      size.append(x[1])
+      sum_qual.append(x[3])
+      sum_nov.append(x[4])
+      max_qual.append(x[7])
+    #   print(myline)
+      i += 1
 
+  return size,sum_qual,sum_nov,max_qual
+
+def plot_navigation():
+
+    ## Navigation
     filenames = [
         ("progress-archive-nosel (2).log", "no_selection"), 
         ("progress-archive-pareto-NSLC (2).log", "NSLC"),
@@ -35,6 +58,8 @@ def main():
         ("progress-archive-score-FIT.log", "fitness"),
         ("progress-archive-score-NS.log", "novelty"),
         ]
+
+
     nb_gen = 1000
     data = {"size":{}, "sum_qual":{}, "sum_nov":{}, "max_qual":{}}
     x = range(nb_gen)
@@ -49,59 +74,146 @@ def main():
         #     print(size)
 
         # print(data['size'][name[1]])
-    # for metric in ['size', 'sum_qual', 'sum_nov', 'max_qual']:
-    #     for name in filenames:
-    #         plt.plot(x, data[metric][name[1]], label=name[1])
-    #     plt.xlabel('Number of Iterations')
-    #     # plt.ylabel('y label')
-    #     plt.title(metric)
-    #     plt.legend()
-    #     plt.grid(True)
-    #     plt.show()
-    metric = 'size'
-    for name in filenames:
-        plt.plot(x, data[metric][name[1]], label=name[1])
-    plt.xlabel('Number of Iterations')
-    # plt.set_xticks(range(0, nb_gen, 100))
-    # plt.ylabel('y label')
-    plt.title(metric)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    for metric in ['size', 'sum_qual', 'sum_nov', 'max_qual']:
+        for name in filenames:
+            plt.plot(x, data[metric][name[1]], label=name[1])
+        plt.xlabel('Number of Iterations')
+        # plt.ylabel('y label')
+        plt.title(metric)
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+    # metric = 'size'
+    # for name in filenames:
+    #     plt.plot(x, data[metric][name[1]], label=name[1])
+    # plt.xlabel('Number of Iterations')
+    # # plt.set_xticks(range(0, nb_gen, 100))
+    # # plt.ylabel('y label')
+    # plt.title(metric)
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
 
-    metric = 'sum_qual'
-    for name in filenames:
-        plt.plot(x, data[metric][name[1]], label=name[1])
-    plt.xlabel('Number of Iterations')
-    # plt.set_xticks(range(0, nb_gen, 100))
-    # plt.ylabel('y label')
-    plt.title(metric)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # metric = 'sum_qual'
+    # for name in filenames:
+    #     plt.plot(x, data[metric][name[1]], label=name[1])
+    # plt.xlabel('Number of Iterations')
+    # # plt.set_xticks(range(0, nb_gen, 100))
+    # # plt.ylabel('y label')
+    # plt.title(metric)
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
 
-    metric = 'sum_nov'
-    for name in filenames:
-        plt.plot(x, data[metric][name[1]], label=name[1])
-    plt.xlabel('Number of Iterations')
-    # plt.set_xticks(range(0, nb_gen, 100))
-    # plt.ylabel('y label')
-    plt.title(metric)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # metric = 'sum_nov'
+    # for name in filenames:
+    #     plt.plot(x, data[metric][name[1]], label=name[1])
+    # plt.xlabel('Number of Iterations')
+    # # plt.set_xticks(range(0, nb_gen, 100))
+    # # plt.ylabel('y label')
+    # plt.title(metric)
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
 
-    metric = 'max_qual'
-    for name in filenames:
-        plt.plot(x, data[metric][name[1]], label=name[1])
-    plt.xlabel('Number of Iterations')
-    # plt.set_xticks(range(0, nb_gen, 100))
-    # plt.ylabel('y label')
-    plt.title(metric)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # metric = 'max_qual'
+    # for name in filenames:
+    #     plt.plot(x, data[metric][name[1]], label=name[1])
+    # plt.xlabel('Number of Iterations')
+    # # plt.set_xticks(range(0, nb_gen, 100))
+    # # plt.ylabel('y label')
+    # plt.title(metric)
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
 
+def plot_robot_arm():
+    
+    ## Navigation
+    filenames = [
+        ("progress-archive-noselection.dat", "no_selection"), 
+        ("progress-archive-pareto.dat", "NSLC"),
+        ("progress-archive-pop-curiosity.dat", "pop_curiosity"),
+        ("progress-archive-pop-fitness.dat", "pop_fitness"),
+        # ("progress-archive-pop-novelty.dat", "pop_novelty"),
+        ("progress-archive-random.dat", "random"),
+        ("progress-archive-curiosity.dat", "curiosity"),
+        ("progress-archive-fitness.dat", "fitness"),
+        ("progress-archive-novelty.dat", "novelty"),
+        ]
+
+    x = range(0, 50000, 1000)
+    data = {"size":{}, "sum_qual":{}, "sum_nov":{}, "max_qual":{}}
+    for name in filenames:
+        print(name)
+        size,sum_qual,sum_nov,max_qual = recup_robotarm(name[0])
+        # try:
+        data['size'][name[1]] = [int(s) for s in size]
+        data['sum_qual'][name[1]] = [-float(s) for s in sum_qual] #sum_qual.copy()
+        data['sum_nov'][name[1]] = [float(s) for s in sum_nov]#sum_nov.copy()
+        data['max_qual'][name[1]] = [-float(s) for s in max_qual]#max_qual.copy()
+        # except ValueError:
+        #     print(sum_qual)
+        #     print(sum_nov)
+        #     print(max_qual)
+
+        # print(data['size'][name[1]])
+    for metric in ['size', 'sum_qual', 'sum_nov', 'max_qual']:
+        for name in filenames:
+            plt.plot(x, data[metric][name[1]], label=name[1])
+        plt.xlabel('Number of Iterations')
+        # plt.ylabel('y label')
+        plt.title(metric)
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+    # metric = 'size'
+    # for name in filenames:
+    #     plt.plot(x, data[metric][name[1]], label=name[1])
+    # plt.xlabel('Number of Iterations')
+    # # plt.set_xticks(range(0, nb_gen, 100))
+    # # plt.ylabel('y label')
+    # plt.title(metric)
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
+
+    # metric = 'sum_qual'
+    # for name in filenames:
+    #     plt.plot(x, data[metric][name[1]], label=name[1])
+    # plt.xlabel('Number of Iterations')
+    # # plt.set_xticks(range(0, nb_gen, 100))
+    # # plt.ylabel('y label')
+    # plt.title(metric)
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
+
+    # metric = 'sum_nov'
+    # for name in filenames:
+    #     plt.plot(x, data[metric][name[1]], label=name[1])
+    # plt.xlabel('Number of Iterations')
+    # # plt.set_xticks(range(0, nb_gen, 100))
+    # # plt.ylabel('y label')
+    # plt.title(metric)
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
+
+    # metric = 'max_qual'
+    # for name in filenames:
+    #     plt.plot(x, data[metric][name[1]], label=name[1])
+    # plt.xlabel('Number of Iterations')
+    # # plt.set_xticks(range(0, nb_gen, 100))
+    # # plt.ylabel('y label')
+    # plt.title(metric)
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
+
+def main():
+    # plot_navigation()
+    plot_robot_arm()
 
 if __name__ == "__main__":
     main()
